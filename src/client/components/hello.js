96da85ceb7d519d7e196fbd65598794af63b98a6
Giuseppe Moi <giuseppemoi@Giuseppes-MacBook-Air.local>
2021-10-08 12:00:01 +0200

    first commit

---
+++ src/client/components/hello.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+const HelloWorld = () => (
+    <div>
+        <h1>{"Hello, world!"}</h1>
+        <hr />
+        <h2> {"salut c'est cool"}</h2>
+        <small>{"becode/trouvkach"}</small>
+    </div>
+);
+
+export default HelloWorld;
